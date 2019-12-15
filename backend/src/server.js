55e39732afaaa3a7699e6a18be612246166e65e3
gilsonDias92 <gilsinho.1992@gmail.com>
2019-12-15 09:39:44 -0300

    Implementação de aprovação de requisição, criacao de controller e configuracao de rotas no backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,26 +1,38 @@
-const express = require('express');
+const express = require("express");
 // express microframework, nos ajuda com rotas e outras funcionalidades..
-const mongoose = require('mongoose');
-const cors = require('cors');
-const path = require('path');
-const socketio = require('socket.io');
-const http = require('http');
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+const socketio = require("socket.io");
+const http = require("http");
 
-const routes = require('./routes');
+const routes = require("./routes");
 // ./pasta indica que é um diretório e nao um módulo (caminho relativo)
 
 const app = express();
 const server = http.Server(app);
 const io = socketio(server);
 
+mongoose.connect(
+  "mongodb+srv://desenvolvedor:desenvolvedor@codeandcoffe-trrr3.mongodb.net/condeAndCoffeDB?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 
-io.on('connection', socket => {
-    console.log('Usuário conectado', socket.id);
+const connectedUsers = {};
+
+io.on("connection", socket => {
+  const { user_id } = socket.handshake.query;
+  connectedUsers[user_id] = socket.id;
 });
 
-mongoose.connect('mongodb+srv://desenvolvedor:desenvolvedor@codeandcoffe-trrr3.mongodb.net/condeAndCoffeDB?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
 });
 
 // passamos rota, funcao para recuperar parametros
@@ -32,7 +44,7 @@ mongoose.connect('mongodb+srv://desenvolvedor:desenvolvedor@codeandcoffe-trrr3.m
 
 app.use(cors());
 app.use(express.json());
-app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
 server.listen(3333);
--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,11 +1,26 @@
-import React, { useEffect, useState } from "react";
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState, useMemo } from "react";
+import { Link } from "react-router-dom";
 import api from "../../services/api";
+import socketio from "socket.io-client";
 
-import './styles.css';
+import "./styles.css";
+import { request } from "http";
 
 export default function Dashboard() {
   const [spots, setSpots] = useState([]);
+  const [requests, setRequests] = useState([]);
+
+  const user_id = localStorage.getItem("user");
+  const socket = useMemo(() => socketio("http://localhost:3333", {
+    query: { user_id }
+  }), [user_id]);
+
+  // dispara a funcao assim q o elemento é exibido em tela
+  useEffect(() => {
+    socket.on("booking_request", data => {
+      setRequests([...requests, data]);
+    });
+  }, [requests, socket]);
 
   useEffect(() => {
     async function loadSpots() {
@@ -20,7 +35,20 @@ export default function Dashboard() {
   }, []);
   return (
     <>
-<h2>SPOTS CADASTRADOS</h2>
+      <ul className="notifications">
+        {requests.map(request => (
+          <li key={request._id}>
+            <p>
+              <strong>{request.user.email}</strong> está solicitando uma reserva
+              em <strong>{request.spot.company}</strong> para a data:
+              <strong>{request.date}</strong>
+            </p>
+            <button className="accept">ACEITAR</button>
+            <button className="reject">RECUSAR</button>
+          </li>
+        ))}
+      </ul>
+
       <ul className="spot-list">
         {spots.map(spot => (
           <li key={spot._id}>
@@ -34,7 +62,7 @@ export default function Dashboard() {
       </ul>
 
       <Link to="/new">
-     <button className="btn"> Cadastrar novo spot </button> 
+        <button className="btn"> Cadastrar novo spot </button>
       </Link>
     </>
   );
